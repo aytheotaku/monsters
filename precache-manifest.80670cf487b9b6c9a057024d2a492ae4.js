38a87ab62ecf47d26a6071bad23e6b3ac74c1563
Ayomide Akinbo <ayakinbo@gmail.com>
2020-07-02 02:00:18 +0100

    Updates

---
+++ precache-manifest.80670cf487b9b6c9a057024d2a492ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f699b46ab44f22b32e0e4d45954ad5d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8736f4a6044b01c80e0c",
+    "url": "/monsters/static/css/main.4735dd4e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8736f4a6044b01c80e0c",
+    "url": "/monsters/static/js/main.cf17df02.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
